f1ce499fd4d733131b2659b517a593209eb6e451
skrpion <nestor.martinez.c@uniautonoma.edu.co>
2023-07-11 23:21:08 -0500

    refactor: storybook table con tres columnas

--- src/environments/environment.stage.ts
+++ src/environments/environment.stage.ts
@@ -4,4 +4,6 @@ export const environment = {
   useHash: false,
   pageSize: 6,
   apiPath: 'https://api-cursoangular.cursos-dev.com',
+  apiPathRandomUser: 'https://randomuser.me/api/',
+  recaptchaKey: '6LcqVe8cAAAAAFKrdDeUow8MCAXl4XGMsm7XbS-T',
 };
