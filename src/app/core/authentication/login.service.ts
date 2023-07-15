36089ccc062deed54942e63bb13218412507a6c7
skrpion <nestor.martinez.c@uniautonoma.edu.co>
2023-07-15 11:40:25 -0500

    refactor: mostrando el menu

--- src/app/core/authentication/login.service.ts
+++ src/app/core/authentication/login.service.ts
@@ -1,11 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
-import { Menu } from '@core';
+import { Menu, MenuService, TokenService } from '@core';
 import { StorageRepository } from 'app/routes/auth/domain/storage-repository';
 import { StorageInfrastructure } from 'app/routes/auth/infrastructure/storage-infrastructure';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable, of, throwError } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 import { Token, User } from './interface';
+import jwtDecode from 'jwt-decode';
 
 @Injectable({
   providedIn: 'root',
@@ -13,7 +14,9 @@ import { Token, User } from './interface';
 export class LoginService {
   constructor(
     protected http: HttpClient,
-    @Inject(StorageInfrastructure) private readonly storageRepository: StorageRepository
+    @Inject(StorageInfrastructure) private readonly storageRepository: StorageRepository,
+    private tokenService: TokenService,
+    private menuService: MenuService
   ) {}
 
   login(email: string, password: string, recaptcha: string, rememberMe = false): Observable<Token> {
@@ -40,10 +43,27 @@ export class LoginService {
   }
 
   me() {
-    return this.http.get<User>('/me');
+    const accessToken = this.tokenService.getBearerToken();
+    try {
+      const decodedToken = jwtDecode(accessToken);
+      const user: User = decodedToken as User;
+
+      if (!user.avatar) {
+        user.avatar = 'assets/images/dreamcode.jpeg';
+      }
+
+      return of(user);
+    } catch (error) {
+      // Manejo de error en caso de token inválido
+      return throwError(new Error('Token inválido'));
+    }
   }
 
-  menu() {
-    return this.http.get<{ menu: Menu[] }>('/me/menu').pipe(map(res => res.menu));
+  menu(): Observable<Menu[]> {
+    return this.http
+      .get<{ menu: Menu[] }>('assets/data/menu.json?_t=' + Date.now())
+      .pipe(map(response => response.menu));
   }
+
+  //return this.http.get<{ menu: Menu[] }>('/me/menu').pipe(map(res => res.menu));
 }
